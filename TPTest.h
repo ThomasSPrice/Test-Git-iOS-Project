56b74724d534b28f7c284f4af82b746e3256371b
Price <thomas.price@TABOUD-PRICETH.local>
2014-04-01 12:31:54 -0600

    added a test class

---
+++ TPTest.h
@@ -0,0 +1,13 @@
+//
+//  TPTest.h
+//  Git Test
+//
+//  Created by Price, Thomas on 4/1/14.
+//  Copyright (c) 2014 Price, Thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPTest : NSObject
+
+@end
---
+++ TPTest.m
@@ -0,0 +1,13 @@
+//
+//  TPTest.m
+//  Git Test
+//
+//  Created by Price, Thomas on 4/1/14.
+//  Copyright (c) 2014 Price, Thomas. All rights reserved.
+//
+
+#import "TPTest.h"
+
+@implementation TPTest
+
+@end
